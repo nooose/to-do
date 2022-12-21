473f7fd1f5e4b71286f62cefd2235cb67badf1b9
nooose <hd15807@gmail.com>
2022-12-21 20:33:25 +0900

    #3 - Body 리팩토링
    본문 내용이 비어있어도 허용하도록 수정

--- src/test/java/com/noose/todo/domain/schedule/BodyTest.java
+++ src/test/java/com/noose/todo/domain/schedule/BodyTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.stream.Stream;
 
@@ -13,23 +13,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("Body 도메인 테스트")
 class BodyTest {
 
-    @DisplayName("null 또는 빈값인 경우 예외가 발생한다")
-    @NullAndEmptySource
+    @DisplayName("null 인 경우 예외가 발생한다")
+    @NullSource
     @ParameterizedTest(name = "입력값: {0}")
     void bodyNullAndEmptyTest(String body) {
         assertThatThrownBy(() -> new Body(body)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("1보다 작거나 500보다 큰 경우 예외가 발생한다.")
+    @DisplayName("길이가 500보다 큰 경우 예외가 발생한다.")
     @ParameterizedTest(name = "입력값: {0}")
     @MethodSource
     void lengthExceptionTest(String body) {
-        assertThatThrownBy(() -> new Body(body)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Body(body))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     private static Stream<Arguments> lengthExceptionTest() {
         return Stream.of(
-                Arguments.of(""),
                 Arguments.of("a".repeat(501))
         );
     }
