30facabb03135b5f3bd4ea3da7225464a0ac393a
nooose <hd15807@gmail.com>
2022-12-21 18:22:00 +0900

    #2 - Schedule, Title 도메인 클래스 추가 (WIP)
    간단하게 Title 만 담을 수 있도록 추가했다.

---
+++ src/main/java/com/noose/todo/domain/schedule/Title.java
@@ -0,0 +1,24 @@
+package com.noose.todo.domain.schedule;
+
+public class Title {
+
+    private static final int MAX_LENGTH = 20;
+
+    private String value;
+
+    public Title(String value) {
+        validate(value);
+
+        this.value = value;
+    }
+
+    private void validate(String title) {
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("제목은 null 또는 빈 문자열이 될 수 없습니다.");
+        }
+
+        if (title.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("제목은 20 글자를 초과할 수 없습니다. 입력한 길이: " + title.length());
+        }
+    }
+}
