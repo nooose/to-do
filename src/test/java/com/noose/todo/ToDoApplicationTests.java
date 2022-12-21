e7443d8f0f8ffa92676695344878bcc91564219e
nooose <hd15807@gmail.com>
2022-12-21 17:06:02 +0900

    #2 - 프로젝트 시작
    스프링부트 3.0.0 으로 프로젝트를 생성한다.

---
+++ src/test/java/com/noose/todo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noose.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
