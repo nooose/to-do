473a2c7d68a30062c6d6a2f6a78719e2f67fd409
nonoro <kgu1022@naver.com>
2022-12-22 00:09:10 +0900

    #3 - Schedule -> ScheduleNote
    startDate 와 endDate 삭제 후 Period 를 추가했다.
    Period 의 기본값으로 시작일과, 종료일에 현재시간이 들어가도록 설정했다.

--- src/test/java/com/noose/todo/domain/schedule/ScheduleNoteTest.java
+++ src/test/java/com/noose/todo/domain/schedule/ScheduleNoteTest.java
@@ -10,36 +10,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("Schedule 도메인 테스트")
-class ScheduleTest {
+class ScheduleNoteTest {
 
     @DisplayName("제목, 내용으로 생성할 수 있다.")
     @Test
     void createScheduleTest() {
-        assertDoesNotThrow(() -> new Schedule("title", "body"));
+        assertDoesNotThrow(() -> new ScheduleNote("title", "body"));
     }
 
     @DisplayName("제목만 입력한 경우 본문 내용은 비어있다.")
     @Test
     void titleOnlyTest() {
-        Schedule schedule = new Schedule("title");
+        ScheduleNote scheduleNote = new ScheduleNote("title");
 
-        assertThat(schedule.isEmptyBody()).isTrue();
+        assertThat(scheduleNote.isEmptyBody()).isTrue();
     }
 
     @DisplayName("제목이 비어 있는 경우 예외가 발생한다.")
     @NullAndEmptySource
     @ParameterizedTest
     void createTitleExceptionScheduleTest(String title) {
-        assertThatThrownBy(() -> new Schedule(title)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new ScheduleNote(title)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("Todo 를 추가할 수 있다.")
     @Test
     void addTodoTest() {
-        Schedule schedule = new Schedule("title", "body");
+        ScheduleNote scheduleNote = new ScheduleNote("title", "body");
 
-        schedule.addTodo(TodoTest.getTodoFixture("할 일"));
+        scheduleNote.addTodo(TodoTest.getTodoFixture("할 일"));
 
-        assertThat(schedule.todoSize()).isEqualTo(1);
+        assertThat(scheduleNote.todoSize()).isEqualTo(1);
     }
 }
