30f1b36fadddf9b2f2d48272b55d99e95891084d
nonoro <kgu1022@naver.com>
2022-12-21 18:25:01 +0900

    #3 - Body 테스트 코드 작성
    테스트 코드에 맞게 Body 도메인을 생성했고
    간단하게 검증로직만 추가함

---
+++ src/main/java/com/noose/todo/domain/schdule/Body.java
@@ -0,0 +1,24 @@
+package com.noose.todo.domain.schdule;
+
+public class Body {
+
+    private static final int MAX_LENGTH = 500;
+
+    private String value;
+
+    public Body(String value) {
+        validate(value);
+
+        this.value = value;
+    }
+
+    private void validate(String body) {
+        if (body == null || body.equals("")) {
+            throw new IllegalArgumentException("글자수는 빈값이거나 null 일 수 없습니다.");
+        }
+
+        if (body.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("본문 내용 길이는" + MAX_LENGTH + "보다 클 수 없습니다.");
+        }
+    }
+}
