f4512ac7bf2bcd4c84cc5be1415b7e80d619c0b2
nonoro <kgu1022@naver.com>
2022-12-22 00:06:34 +0900

    #3 - Period 클래스 추가 및 Period 테스트코드 작성
    startDate와 endDate를 관리할 수 있는
    Period 클래스를 생성했다.

---
+++ src/main/java/com/noose/todo/domain/schedule/Period.java
@@ -0,0 +1,26 @@
+package com.noose.todo.domain.schedule;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Period {
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public Period() {
+        LocalDateTime now = LocalDateTime.now();
+        this.startDate = now;
+        this.endDate = now;
+    }
+
+    public Period(LocalDateTime startDate, LocalDateTime endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("종료일은 시작일보다 빠를 수 없습니다.");
+        }
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
