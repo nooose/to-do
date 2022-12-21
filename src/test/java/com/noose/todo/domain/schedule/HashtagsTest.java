83f6ba9dcc292628a90d038195bd10ca920a9cce
nooose <hd15807@gmail.com>
2022-12-22 02:20:07 +0900

    #3 - 해시태그 기능 구현
    해시태그를 여러개 관리하는 일급 컬렉션을 생성했고
    일급컬렉션 내에서 본문 내용을 파싱하여 여러 해시태그를 추출할 수 있도록 구현했다.
    
    또한 ScheduleNote 객체 생성을 편리하게 하기 위해 생성자 대신 정적 팩터리 메서드를 사용하도록 변경했다.
    
    이에 맞는 테스트 코드도 같이 작성했다.

---
+++ src/test/java/com/noose/todo/domain/schedule/HashtagsTest.java
@@ -0,0 +1,48 @@
+package com.noose.todo.domain.schedule;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("Hashtag 일급 컬렉션 테스트")
+class HashtagsTest {
+
+    @DisplayName("본문 내용에서 해시태그를 추출할 수 있다.")
+    @ParameterizedTest(name = "{0} --> {1}")
+    @MethodSource
+    void extractHashtagsTest(Body body, Set<String> hashtags) {
+        Set<String> result = new Hashtags(body).getValues()
+                .stream()
+                .map(Hashtag::getHashtagName)
+                .collect(Collectors.toSet());
+
+        assertThat(result).isEqualTo(hashtags);
+    }
+
+    static Stream<Arguments> extractHashtagsTest() {
+        return Stream.of(
+                arguments("", Set.of()),
+                arguments(" ", Set.of()),
+                arguments("#", Set.of()),
+                arguments("# ", Set.of()),
+                arguments(" #", Set.of()),
+                arguments(" # ", Set.of()),
+                arguments("#안녕", Set.of("안녕")),
+                arguments("#안녕_Hello", Set.of("안녕_Hello")),
+                arguments("#안녕#하세요", Set.of("안녕", "하세요")),
+                arguments("#안녕 #하세요", Set.of("안녕", "하세요")),
+                arguments("#안녕 #하세요#안녕 #안녕", Set.of("안녕", "하세요")),
+                arguments("테스트 #안녕 #하세요 테스트", Set.of("안녕", "하세요")),
+                arguments("테스트 #good #test 테스트", Set.of("good", "test")),
+                arguments("테스트 #good #test #A123 테스트", Set.of("good", "test", "A123"))
+        );
+    }
+}
--- src/test/java/com/noose/todo/domain/schedule/ScheduleNoteTest.java
+++ src/test/java/com/noose/todo/domain/schedule/ScheduleNoteTest.java
@@ -5,42 +5,59 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DisplayName("Schedule 도메인 테스트")
+@DisplayName("ScheduleNote 도메인 테스트")
 class ScheduleNoteTest {
 
-    @DisplayName("제목, 내용으로 생성할 수 있다.")
-    @Test
-    void createScheduleTest() {
-        assertDoesNotThrow(() -> new ScheduleNote("title", "body"));
+    @DisplayName("제목이 비어 있는 경우 예외가 발생한다.")
+    @NullAndEmptySource
+    @ParameterizedTest(name = "입력값: {0}")
+    void createTitleExceptionScheduleTest(String title) {
+        assertThatThrownBy(() -> ScheduleNote.of(title))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("제목만 입력한 경우 본문 내용은 비어있다.")
     @Test
     void titleOnlyTest() {
-        ScheduleNote scheduleNote = new ScheduleNote("title");
+        ScheduleNote scheduleNote = ScheduleNote.of("title");
 
         assertThat(scheduleNote.isEmptyBody()).isTrue();
-    }
-
-    @DisplayName("제목이 비어 있는 경우 예외가 발생한다.")
-    @NullAndEmptySource
-    @ParameterizedTest
-    void createTitleExceptionScheduleTest(String title) {
-        assertThatThrownBy(() -> new ScheduleNote(title))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("Todo 를 추가할 수 있다.")
     @Test
     void addTodoTest() {
-        ScheduleNote scheduleNote = new ScheduleNote("title", "body");
+        ScheduleNote scheduleNote = ScheduleNote.of("title", "body");
 
         scheduleNote.addTodo(TodoTest.getTodoFixture("할 일"));
 
         assertThat(scheduleNote.todoSize()).isEqualTo(1);
+    }
+
+
+    @DisplayName("제목과 본문으로 생성하는 경우 해시태그, 기한이 설정된다.")
+    @Test
+    void createScheduleNote() {
+        String title = "테스트";
+        String body = "테스트 글 #테스트 #first #test";
+        LocalDateTime baseTime = LocalDateTime.now();
+        ScheduleNote scheduleNote = ScheduleNote.of(title, body);
+
+        assertAll(() -> {
+            assertThat(scheduleNote.getTitle()).isEqualTo(new Title(title));
+            assertThat(scheduleNote.getBody()).isEqualTo(new Body(body));
+            assertThat(scheduleNote.getHashtags().hashtags()).isEqualTo(Set.of("테스트", "first", "test"));
+            assertThat(scheduleNote.todoSize()).isEqualTo(0);
+            assertThat(scheduleNote.getPeriod().getStartDate()).isAfter(baseTime);
+        });
+
+        System.out.println(scheduleNote);
     }
 }
