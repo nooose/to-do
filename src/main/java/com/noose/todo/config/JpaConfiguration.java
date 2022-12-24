f7da70afa99048a8dad3ac52736eb68286898d45
nooose <hd15807@gmail.com>
2022-12-24 15:59:51 +0900

    #17 - 부적절한 패키지 경로 수정
    도메인 외부 설정 클래스가 모인 패키지이므로 domain 바깥에 위치시킨다.

--- src/main/java/com/noose/todo/config/JpaConfiguration.java
+++ src/main/java/com/noose/todo/config/JpaConfiguration.java
@@ -1,4 +1,4 @@
-package com.noose.todo.domain.config;
+package com.noose.todo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
